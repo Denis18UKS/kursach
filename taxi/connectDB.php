7843c25077aa1641066e5bb989b074fbb87aee8f
Denis18UKS <148848338+Denis18UKS@users.noreply.github.com>
2024-04-23 23:23:38 +0500

    Delete taxi directory

--- taxi/connectDB.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $connect = mysqli_connect("localhost", "root", "", "taxi");
-?>
-
-<!-- SELECT * FROM users INNER JOIN orders ON users.id = orders.id_user JOIN tarifs ON tarifs.id = orders.id_tarifs; -->
